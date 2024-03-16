59ff5008f668243da0f9175f34255bc9a9859997
prkhar <prkharpatel20@gmail.com>
2024-03-16 16:16:34 +1100

    Resource creation through terraform

---
+++ 02-resource.tf/ec2.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "frontend" {
+  ami           = "ami-05f020f5935e52dc4"
+  instance_type = "t3.micro"
+  tags = {
+    name = "frontend"
+  }
+  security_groups = ["allow-all"]
+
+}
