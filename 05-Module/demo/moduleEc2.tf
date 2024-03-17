615989a15e0ce5247efafa6e94d27ea9392fe8d6
prkhar <prkharpatel20@gmail.com>
2024-03-17 23:18:40 +1100

    terraform module

--- 05-Module/demo/moduleEc2.tf
+++ 05-Module/demo/moduleEc2.tf
@@ -1,11 +1,11 @@
 resource "aws_instance" "demo1" {
-  ami           = data.aws_instance.selected.ami
+  #ami           = data.aws_instance.selected.ami
+  ami = "ami-05f020f5935e52dc4"
   instance_type = "t3.micro"
-  Name = "demo-1"
 }
 
-data "aws_instance" "selected" {
-  tags = {
-    Name = "workstation"
-  }
-}
+#data "aws_instance" "selected" {
+#  tags = {
+#    Name = "workstation"
+#  }
+#}
