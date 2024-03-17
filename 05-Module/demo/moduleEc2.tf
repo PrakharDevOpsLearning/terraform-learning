4d6da3e85d45ed6fb3e2c39c4d6e221d5d40c48c
prkhar <prkharpatel20@gmail.com>
2024-03-17 23:31:21 +1100

    terraform module

--- 05-Module/demo/moduleEc2.tf
+++ 05-Module/demo/moduleEc2.tf
@@ -1,11 +1,15 @@
 resource "aws_instance" "demo1" {
   #ami           = data.aws_instance.selected.ami
   ami = "ami-05f020f5935e52dc4"
-  instance_type = "t3.micro"
+  instance_type = var.Instance
+  tags = {
+    Name = var.Name
+  }
+  security_groups = [var.secGrp]
+
 }
 
-#data "aws_instance" "selected" {
-#  tags = {
-#    Name = "workstation"
-#  }
-#}
+
+variable "Name" {}
+variable "Instance" {}
+variable "secGrp" {}
