0d8f900c3b0eca2577d57ce3150d9d6eaae82041
prkhar <prkharpatel20@gmail.com>
2024-03-19 09:00:43 +1100

    terraform module

--- 05-Module/main.tf
+++ 05-Module/main.tf
@@ -1,16 +1,24 @@
 module "demo-1" {
   source = "./demo"
-  Name = "DEMO-1"
-  Instance =  "t2.micro"
+  Name = "Frontend"
+  Instance =  "t3.small"
   secGrp = data.aws_security_group.selected.id
 }
 
 module "demo-2" {
   source = "./demo"
-  Name = "DEMO-2"
-  Instance = "t3.micro"
+  Name = "Backend"
+  Instance = "t3.small"
   secGrp = data.aws_security_group.selected.id
 }
+
+module "demo-3" {
+  source = "./demo"
+  Name = "Mysql"
+  Instance = "t3.small"
+  secGrp = data.aws_security_group.selected.id
+}
+
 
 data "aws_security_group" "selected" {
   name = "allow-all"
